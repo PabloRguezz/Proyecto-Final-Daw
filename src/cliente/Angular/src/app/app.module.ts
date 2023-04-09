327d8e4b67364bffcd0ecc4b0702dda572e3d45c
PabloRguezz <pablorodirguezz@gmail.com>
2023-04-09 14:18:27 +0100

    Solucionando errores #28

--- src/cliente/Angular/src/app/app.module.ts
+++ src/cliente/Angular/src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from '../components/login/login.component';
 import { RegistroComponent } from 'src/components/registro/registro.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegistroComponent
+    RegistroComponent,
+    FormsModule,
+    BrowserModule
   ],
   imports: [
     BrowserModule,
