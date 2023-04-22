9332bab5c71cc51dd23932bf153a644120e89c84
PabloRguezz <pablorodirguezz@gmail.com>
2023-04-22 12:53:24 +0100

    Añadiendo registro Empresa #34

--- src/cliente/Angular/src/components/registro/registro.component.ts
+++ src/cliente/Angular/src/components/registro/registro.component.ts
@@ -23,7 +23,7 @@ export class RegistroComponent {
       (response) => {
       if (response['status']) {
         Swal.fire({
-          position: 'top-end',
+          position: 'center',
           icon: 'success',
           title: response['msg'],
           showConfirmButton: false,
@@ -45,7 +45,98 @@ export class RegistroComponent {
       })
     });
   }
+  obtenerHorasDia(fecha) {
+    const dias = ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'];
+    const horas = fecha.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'});
+    const dia = dias[fecha.getDay()];
+    return `${dia} ${horas}`;
+  }
+  horarioToString(horario) {
+    if (!horario || horario === " - ") {
+      return "Cerrado";
+    } else {
+      return horario;
+    }
+  }
   
+  registrarEmpresa() {
+      const nombreEmpresaInput = document.getElementById('nombreEmpresa') as HTMLInputElement;
+      const cifInput = document.getElementById('cif') as HTMLInputElement;
+      const telefonoInput = document.getElementById('telefono') as HTMLInputElement;
+      const lunesInicioInput = document.getElementById('lunesInicio') as HTMLInputElement;
+      const lunesFinInput = document.getElementById('lunesFin') as HTMLInputElement;
+      const martesInicioInput = document.getElementById('martesInicio') as HTMLInputElement;
+      const martesFinInput = document.getElementById('martesFin') as HTMLInputElement;
+      const miercolesInicioInput = document.getElementById('miercolesInicio') as HTMLInputElement;
+      const miercolesFinInput = document.getElementById('miercolesFin') as HTMLInputElement;
+      const juevesInicioInput = document.getElementById('juevesInicio') as HTMLInputElement;
+      const juevesFinInput = document.getElementById('juevesFin') as HTMLInputElement;
+      const viernesInicioInput = document.getElementById('viernesInicio') as HTMLInputElement;
+      const viernesFinInput = document.getElementById('viernesFin') as HTMLInputElement;
+      const sabadoInicioInput = document.getElementById('sabadoInicio') as HTMLInputElement;
+      const sabadoFinInput = document.getElementById('sabadoFin') as HTMLInputElement;
+      const domingoInicioInput = document.getElementById('domingoInicio') as HTMLInputElement;
+      const domingoFinInput = document.getElementById('domingoFin') as HTMLInputElement;
+      const ubicacionInput = document.getElementById('ubicacion') as HTMLInputElement;
+      const descripcionInput = document.getElementById('descripcion') as HTMLInputElement;
+      const passwordInput = document.getElementById('password') as HTMLInputElement;
+    
+      const nombreEmpresa = nombreEmpresaInput.value;
+      const cif = cifInput.value;
+      const telefono = telefonoInput.value;
+      const lunesInicio = lunesInicioInput.value;
+      const lunesFin = lunesFinInput.value;
+      const martesInicio = martesInicioInput.value;
+      const martesFin = martesFinInput.value;
+      const miercolesInicio = miercolesInicioInput.value;
+      const miercolesFin = miercolesFinInput.value;
+      const juevesInicio = juevesInicioInput.value;
+      const juevesFin = juevesFinInput.value;
+      const viernesInicio = viernesInicioInput.value;
+      const viernesFin = viernesFinInput.value;
+      const sabadoInicio = sabadoInicioInput.value;
+      const sabadoFin = sabadoFinInput.value;
+      const domingoInicio = domingoInicioInput.value;
+      const domingoFin = domingoFinInput.value;
+      const ubicacion = ubicacionInput.value;
+      const descripcion = descripcionInput.value;
+      const password = passwordInput.value;
+    
+      const empresa = {
+        nombre: nombreEmpresa,
+        cif_Empresa: cif,
+        tlf_contacto: telefono,
+        horario: `Lun ${this.horarioToString(`${lunesInicio} - ${lunesFin}`)}, Mar ${this.horarioToString(`${martesInicio} - ${martesFin}`)}, Mie ${this.horarioToString(`${miercolesInicio} - ${miercolesFin}`)}, Jue ${this.horarioToString(`${juevesInicio} - ${juevesFin}`)}, Vie ${this.horarioToString(`${viernesInicio} - ${viernesFin}`)}, Sab ${this.horarioToString(`${sabadoInicio} - ${sabadoFin}`)}, Dom ${this.horarioToString(`${domingoInicio} - ${domingoFin}`)}`,
+        ubicacion: ubicacion,
+        descripcion: descripcion,
+        password: password,
+      };
+      this.empresaService.registrarEmpresa(empresa).subscribe(
+        (datos) => {
+          if (datos['status']) {
+            Swal.fire({
+              position: 'center',
+              icon: 'success',
+              title: datos['msg'],
+              showConfirmButton: false,
+              timer: 1500
+            })
+          } else {
+            Swal.fire({
+              icon: 'error',
+              title: datos['msg'],
+            })
+          }
+        },
+        (error) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Ha habido un error registrando la empresa',
+          })
+        }
+      );
+    }
     
   ngOnInit(){
     let signup = document.querySelector(".signup");
