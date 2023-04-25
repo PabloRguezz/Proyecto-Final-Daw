dea5970c420ec7a7bb3bbafd7754d471c8cbf070
PabloRguezz <pablorodirguezz@gmail.com>
2023-04-25 14:54:40 +0100

    Editando acceso Usuario #34

--- src/servidor/Controller/UsuarioController.php
+++ src/servidor/Controller/UsuarioController.php
@@ -1,8 +1,6 @@
 <?php
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Access-Control-Allow-Origin: *');
 
 require_once '../Model/Conexion.php';
 $conn = Conexion::connection();
