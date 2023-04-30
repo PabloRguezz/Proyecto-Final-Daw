8d1ebd18102bf016c511f1589bfc1181d2030b39
PabloRguezz <pablorodirguezz@gmail.com>
2023-04-30 17:10:50 +0100

    Editado empresa y usuario #41

--- src/servidor/Controller/usuario.php
+++ src/servidor/Controller/usuario.php
@@ -4,11 +4,28 @@ require_once("../config/conexion.php");
 require_once("../Model/Usuario.php");
 
 $usuario = new Usuario();
+$body = json_decode(file_get_contents("php://input"),true);
 
 switch ($_GET["user"]) {
     case 'GetAll':
         $datos=$usuario->get_usuario();
         echo json_encode($datos);
         break;
-
+    case 'GetEmail':
+        $datos=$usuario->get_usuario_id($body["email"]);
+        echo json_encode($datos);
+        break;
+    case "insert":
+        $datos=$usuario->insert_usuario($body["email"],$body["password"],$body["nombre"]);
+        echo json_encode("El usuario se ha insertado correctamente");
+        break;
+    case "update":
+        $datos=$usuario->update_usuario($body["id"],$body["email"],$body["password"],$body["nombre"]);
+        echo json_encode("El usuario se ha actualizado correctamente");
+        break;
+    case "delete":
+        $datos=$usuario->delete_usuario($body["id"]);
+        echo json_encode("El usuario se ha eliminado correctamente");
+        break;
 }
+?>
--- src/servidor/Model/Usuario.php
+++ src/servidor/Model/Usuario.php
@@ -1,53 +1,58 @@
 <?php
 class Usuario extends Conexion{
-  private $id_usuario;
-  private $email;
-  private $password;
-  private $nombre;
   public function get_usuario(){
     $conectar=parent::connection();
     parent::set_name();
   
-    $sql = "SELECT * FROM usuario WHERE id_usuario=2";
+    $sql = "SELECT * FROM usuario";
     $sql=$conectar->prepare($sql);
     $sql->execute();
     return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
   }
+
+  public function get_usuario_id($email){
+    $conectar=parent::connection();
+    parent::set_name();
+    $sql = "SELECT * FROM usuario WHERE email=?";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1,$email);
+    $sql->execute();
+    return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function insert_usuario($email,$password,$nombre){
+    $conectar=parent::connection();
+    parent::set_name();
+    $sql = "INSERT INTO usuario(id_usuario,email,password,nombre) VALUES (NULL,?,?,?)";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1,$email);
+    $sql->bindValue(2,$password);
+    $sql->bindValue(3,$nombre);
+    $sql->execute();
+    return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function update_usuario($id,$email,$password,$nombre){
+    $conectar=parent::connection();
+    parent::set_name();
+    $sql = "UPDATE usuario set email=? , pasword=?, nombre=? WHERE id_usuario=?";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1,$email);
+    $sql->bindValue(2,$password);
+    $sql->bindValue(3,$nombre);
+    $sql->bindValue(4,$id);
+    $sql->execute();
+    return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function delete_usuario($id){
+    $conectar=parent::connection();
+    parent::set_name();
+    $sql = "DELETE FROM usuario WHERE id_usuario=?";
+    $sql=$conectar->prepare($sql);
+    $sql->bindValue(1,$id);
+    $sql->execute();
+    return $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+  }
 }
-  /*
-  public function __construct($id_usuario, $email, $password, $nombre) {
-    $this->id_usuario = $id_usuario;
-    $this->email = $email;
-    $this->password = $password;
-    $this->nombre = $nombre;
-  }
-
-  public function getIdUsuario() {
-    return $this->id_usuario;
-  }
-
-  public function getEmail() {
-    return $this->email;
-  }
-
-  public function getPassword() {
-    return $this->password;
-  }
-
-  public function getNombre() {
-    return $this->nombre;
-  }
-
-  public function setEmail($email) {
-    $this->email = $email;
-  }
-
-  public function setPassword($password) {
-    $this->password = $password;
-  }
-
-  public function setNombre($nombre) {
-    $this->nombre = $nombre;
-  }
-}
-/*
+?>
